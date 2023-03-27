46e8d151b788c09210191b4d258480ecc2b9696b
gudyfr <106124392+gudyfr@users.noreply.github.com>
2023-03-27 09:52:24 -0700

    Added a border and generated textures

---
+++ images/map-tiles/frosthaven/repack.bat
@@ -0,0 +1,57 @@
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-01a-snow.png temp/fh-01b-metal.png output/fh-01ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-01c-snow.png temp/fh-01d-metal.png output/fh-01cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-01e-snow.png temp/fh-01f-cave.png output/fh-01ef.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-01g-snow.png temp/fh-01h-dungeon.png output/fh-01gh.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-02a-dungeon.png temp/fh-02b-wood.png output/fh-02ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-02c-dungeon.png temp/fh-02d-wood.png output/fh-02cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-02e-snow.png temp/fh-02f-metal.png output/fh-02ef.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-02g-snow.png temp/fh-02h-metal.png output/fh-02gh.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-02i-dungeon.png temp/fh-02j-cave.png output/fh-02ij.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-02k-dungeon.png temp/fh-02l-cave.png output/fh-02kl.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-03a-cave.png temp/fh-03b-metal.png output/fh-03ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-03c-dungeon.png temp/fh-03d-snow.png output/fh-03cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-04a-cave.png temp/fh-04b-metal.png output/fh-04ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-04c-snow.png temp/fh-04d-dungeon.png output/fh-04cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-05a-cave.png temp/fh-05b-snow.png output/fh-05ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-06a-snow.png temp/fh-06b-metal.png output/fh-06ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-07a-cave.png temp/fh-07b-snow.png output/fh-07ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-07c-cave.png temp/fh-07d-snow.png output/fh-07cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-07e-dungeon.png temp/fh-07f-metal.png output/fh-07ef.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-07g-wood.png temp/fh-07h-metal.png output/fh-07gh.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-08a-metal.png temp/fh-08b-cave.png output/fh-08ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-09a-snow.png temp/fh-09b-dungeon.png output/fh-09ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-09c-metal.png temp/fh-09d-wood.png output/fh-09cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-10a-metal.png temp/fh-10b-dungeon.png output/fh-10ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-10c-metal.png temp/fh-10d-snow.png output/fh-10cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-11a-snow.png temp/fh-11b-cave.png output/fh-11ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-11c-snow.png temp/fh-11d-cave.png output/fh-11cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-11e-dungeon.png temp/fh-11f-metal.png output/fh-11ef.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-12a-metal.png temp/fh-12b-cave.png output/fh-12ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-12c-metal.png temp/fh-12d-dungeon.png output/fh-12cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-13a-snow.png temp/fh-13b-cave.png output/fh-13ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-13c-snow.png temp/fh-13d-cave.png output/fh-13cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-13e-dungeon.png temp/fh-13f-metal.png output/fh-13ef.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-14a-wood.png temp/fh-14b-cave.png output/fh-14ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-15a-snow.png temp/fh-15b-cave.png output/fh-15ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-15c-dungeon.png temp/fh-15d-metal.png output/fh-15cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-16a-snow.png temp/fh-16b-cave.png output/fh-16ab.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-16c-dungeon.png temp/fh-16d-metal.png output/fh-16cd.png
+magick montage -background transparent -geometry +0+0 -tile 1x2 temp/fh-11e-dungeon.png temp/fh-11f-metal.png output/fh-11ef.png
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
